4bcc10125ccd8bd498b1b06a87ff5d337edf6e35
Nsikak-mbre <tm260715@gmail.com>
2023-07-15 16:46:41 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  *  *  *main -is the entry point for the program.
+ *   *   *   *
+ *    *    *    *Return:lowercase alphabet
+ **/
+int main()
+{
+char ch;
+for(ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
