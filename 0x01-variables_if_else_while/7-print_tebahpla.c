f324fdce43bae6fd9f0da784f53a4eb791b98496
Nsikak-mbre <tm260715@gmail.com>
2023-07-17 04:06:20 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * *  *main -is the entry point for the program.
+ *   *   *   *   *
+ *    *    *Return:lowercase alphabet reverse
+ **/
+int main(void)
+{
+char ch;
+for (ch = 'z' ; ch <= 'a' ; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
