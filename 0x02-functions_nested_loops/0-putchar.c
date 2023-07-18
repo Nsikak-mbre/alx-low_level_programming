794f38d97b73941ad1c383e1964072f3ed112a51
Nsikak-mbre <tm260715@gmail.com>
2023-07-18 21:25:30 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio>
+#include "main.h"
+/**
+ * * main - print putchar followed by a new line
+ * * Return: 0 (success)
+ */
+
+int main(void)
+{
+char ch = "_putchar\n";
+return (0);
+}
