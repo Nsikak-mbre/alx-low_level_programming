bb1faaa10999cb7ed5feaa7b6adaa6c7c828754e
Nsikak-mbre <tm260715@gmail.com>
2023-07-18 22:15:01 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
 #include "main.h"
-#define MAXSTRING 80
 /**
  * * main - print putchar followed by a new line
  * * Return: 0 (success)
  */
-
 int main(void)
 {
-char message[] = "_putchar";
-for (int count =  0; count < MAXSTRING; count++)
-{
-if (message[count] == '\0')
-{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
 putchar('\n');
-break;
-}
-else
-putchar(message[count]);
-}
 return (0);
 }
+
