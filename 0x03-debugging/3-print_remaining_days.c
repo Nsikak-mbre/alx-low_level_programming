fd9dbb0bd5cec5dac9682c658b8987765b7b9acc
Nsikak-mbre <tm260715@gmail.com>
2023-07-20 21:52:40 +0000

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,10 +16,10 @@ if ((year % 100 == 0 && year % 400 == 0) || !(year % 400 == 0))
 {
 if (month >= 2 && day >= 60)
 {
+day++;
+}
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
-}
-day++;
 }
 else
 {
