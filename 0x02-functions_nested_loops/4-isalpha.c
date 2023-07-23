dbd0ab653543ff14cf4f10066b34969ef486481c
Nsikak-mbre <tm260715@gmail.com>
2023-07-23 16:30:34 +0000

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  *  * _isalpha - checks for alphanumeric character
+ *   *   * @c: the character
+ *    *    * Return: 1 if the character is lowercase, 0 if not
+ */
+int _isalpha(int c)
+{
+if (isalpha(c) != 0)
+return (1);
+else
+return (0);
+}
