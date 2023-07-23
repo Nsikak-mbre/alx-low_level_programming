82558a0581cf98b91593262eb18823a3d66b4329
Nsikak-mbre <tm260715@gmail.com>
2023-07-23 07:28:39 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-char word [] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r',"\n"};
+char word [] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r',\n};
 int i;
 for (i = 0 ; i <= 8 ; i++)
 {
