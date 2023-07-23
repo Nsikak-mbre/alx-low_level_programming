356bb43112e755686dfbd7c2dc6beac7adfd569b
Nsikak-mbre <tm260715@gmail.com>
2023-07-23 10:05:32 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  *  * print_alphabet - prints _lowercase and a new line
+ *   *   *
+ *    *    * Return: o
+ */
+void print_alphabet(void)
+{
+char lowerCasing;
+for (lowerCasing = 'a'; lowerCasing <= 'z'; lowerCasing++)
+_putchar(lowerCasing);
+{
+_putchar('\n');
+return (0);
+}
