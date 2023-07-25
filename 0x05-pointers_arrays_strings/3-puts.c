234d463eefb732a75f05ad626a06b24dc382759d
Nsikak-mbre <tm260715@gmail.com>
2023-07-25 09:59:17 +0000

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  *  * _puts - prints string followed by new line
+ *   *   * @str: the character
+ *    *    * Return: 0
+ **/
+
+void _puts(char *str);
+{
+while (*str)
+{
+putchar(*str);
+}
+putchar('\n');
+return (0);
+}
