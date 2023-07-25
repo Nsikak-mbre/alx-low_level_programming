b184277d40b295a2cd7e6fb23fa08e84f6583bb8
Nsikak-mbre <tm260715@gmail.com>
2023-07-25 08:33:57 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+
+/**
+ *  *  * _strlen - checks for string length
+ *   *   * @s: the argument
+ *    *    * Return: 0
+ */
+int _strlen(char *s)
+{strlen(*s)
+return (0);
+}
+
+
