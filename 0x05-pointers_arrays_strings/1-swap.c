b877247430e3d1274d1655c43f5a2651b6f325e3
Nsikak-mbre <tm260715@gmail.com>
2023-07-25 01:36:48 +0000

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  *  * swap_int - swaps values
+ *   *   * @a: the character
+ *   *@b: the second argument
+ *    *    * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+if ((*a) && (*b))
+{
+*a = *b && *b = *a
+};
+return (0);
+}
