8f8df6f5953cbbc64f2538c573c7c0ada16ac4c0
Nsikak-mbre <tm260715@gmail.com>
2023-07-26 20:28:07 +0000

    3-print_number.c

---
+++ 0x04-more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  *  * print_numbers - checks for lowercase character
+ *    *    *Return:0
+ **/
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
+}
