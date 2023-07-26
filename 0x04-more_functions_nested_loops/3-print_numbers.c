d2f019f74392e92c42c1d6e2da5ce355639fc89a
Nsikak-mbre <tm260715@gmail.com>
2023-07-26 21:03:08 +0000

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *  *  *  * print_numbers - checks for lowercase character
+ *  *  *  * print_numbers 0-9
  *   *    *    *Return:0
  **/
 void print_numbers(void)
 {
-for (i = 0; i <= 9; i++)
+int i;
+for (i = 0; i <= 10; i++)
 {
-_putchar('0' + i);
+_putchar(i + '0');
 }
 _putchar('\n');
 }
