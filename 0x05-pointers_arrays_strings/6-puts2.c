cf48622a7cb877d9587b654c4541eece66278f24
Nsikak-mbre <tm260715@gmail.com>
2023-07-26 04:57:12 +0000

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  *  *puts2 - prints every other number
+ *   *   * @str: the character
+ *    *    * Return: void function
+ **/
+void puts2(char *str);
+{
+int = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+putchar('\n');
+}
