ff6fa59a68d8fa4122f725ed75b99dbf004b5b58
Nsikak-mbre <tm260715@gmail.com>
2023-07-26 16:02:10 +0000

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *  *  * _isdigit - checks for lowercase character
+ *   *   * @c: the character
+ *    *    * Return: 1 if the character is lowercase, 0 if not
+ **/
+int _isdigit(int c)
+{
+return ((c >= 0 && c <= 9) ? 1 : 0);
+}
