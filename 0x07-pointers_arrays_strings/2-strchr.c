697030aad38348897278d195e7478649ce4400c0
Nsikak-mbre <tm260715@gmail.com>
2023-08-01 02:26:55 +0000

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *  *  * _strchr - select part of string after char
+ *   *   * @s: string to check
+ *    *    * @c: character to look for
+ *     *     *
+ *      *      * Return: s at comparison is success, NULL is char not found
+ **/
+
+char *_strchr(char *s, char c)
+{
+do {
+if (*s == c)
+{
+return (s);
+}
+s++;
+} while (*s != '\0');
+return (NULL);
+}
