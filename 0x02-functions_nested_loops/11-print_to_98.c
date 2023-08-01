b50c375600a7f8d7d8955e8601369b7a898eb062
Nsikak-mbre <tm260715@gmail.com>
2023-08-01 20:38:58 +0000

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- *  *  * print_to_98 - print sum of diagonals in an array
+ *  *  * print_to_98 - print natural numbers from n to 98,
  *   *   * @n: argument
  *     *     *
  *      *      * Return: void
@@ -10,11 +10,25 @@
 
 void print_to_98(int n)
 {
-int i = n;
-while (i <= 98)
+int i, j;
+if (n <= 98)
 {
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
 printf("%d, ", i);
-i++;
+else if (i == 98)
+printf("%d\n", i);
 }
-_putchar('\n');
+}
+else if (n >= 98)
+{
+for (j = n; j >= 98; j--)
+{
+if (j != 98)
+printf("%d, ", j);
+else if (j == 98)
+printf("%d\n", j);
+}
+}
 }
