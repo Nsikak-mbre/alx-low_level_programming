a7bb4fe60edfcc383568fe56544ac9dbf97a50c3
Nsikak-mbre <tm260715@gmail.com>
2023-08-04 05:51:51 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- *  *  * FizzBuzz - buzz-words as indicated
+ *  *  *main - buzz-words as required
  *   *     *
  *    *      * Return: void
  **/
@@ -13,11 +13,11 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+if (i % 3 == 0 && i % 5 != 0)
 {
 printf("Fizz ");
 }
-else if (i % 5 == 0)
+else if (i % 5 == 0 && i % 3 != 0) 
 {
 printf("Buzz ");
 }
