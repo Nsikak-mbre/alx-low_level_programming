38ab310eda08dcb865ec1111c70e253593c10149
Nsikak-mbre <tm260715@gmail.com>
2023-08-07 13:23:17 +0000

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -Wno-unused-variable -c *.c
+gcc -Wall -pedantic -Werror -Wextra -Wno-unused-parameter -Wno-unused-result -c *.c
 ar -rc liball.a *.o
 ranlib liball.a
