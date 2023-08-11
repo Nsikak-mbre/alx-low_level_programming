297645b0ed96831389e2ecde584cd18e6611d7df
Nsikak-mbre <tm260715@gmail.com>
2023-08-11 09:52:22 +0000

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -25,7 +25,7 @@ int _sqrt_recursion(int n)
 {
 if (n <= 0)
 return (-1);
-if( n == 1)
+if (n == 1)
 return (1);
 return (squares(n, 1));
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+   *  *  *  *_is_prime_number - prime numbers
+   *   *   *   * @n: the argument
+   *    *    Return: returns prime numbers
+**/
+
+int is_prime_number(int n)
+{
+
