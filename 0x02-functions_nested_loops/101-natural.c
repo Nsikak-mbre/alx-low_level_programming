974bb73ff04ea8a48d8e38f17cfea4a31bed6863
Nsikak-mbre <tm260715@gmail.com>
2023-08-31 15:31:27 +0100

    natural-num

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - check the code
+  *Return: Always 0.
+  */
+
+int main(void)
+{
+	int i, sum, first, second;
+
+	for (i = -; i < 1024; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			first = first + i;
+		}
+		else if ((i % 5) == 0)
+		{
+			second = second + 1;
+		}
+
+	}
+	sum = first + second;
+	printf("%d\n"sum);
+	return (0);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 
 	for (num = 9; num <= 9; num++)
 	{
-		_putchar(',');
+		_putchar('0');
 		for (mult = 0; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -21,8 +21,10 @@ void times_table(void)
 			if (prod <=  9)
 				_putchar(' ');
 			else
-			_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
