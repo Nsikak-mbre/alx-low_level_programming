3cc127b92fe7a019afe693f08ed0160fd87b4852
Nsikak-mbre <tm260715@gmail.com>
2023-10-11 16:09:25 +0100

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -23,5 +23,17 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-	return (0);
+
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (0);
+	}
+	else if ( *s1 == '\0')
+	{
+		return (-15);
+	}
+	else 
+	{
+		return (15);
+	}
 }
