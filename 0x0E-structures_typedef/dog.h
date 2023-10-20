44f3ed34c5b173d8705b278e153ad4e598d8ea02
Nsikak-mbre <tm260715@gmail.com>
2023-10-20 12:04:09 +0100

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,6 +7,7 @@
  * @age: member
  * @owner: member
  */
+typedef struct dog dog_t;
 
 struct dog
 {
@@ -15,5 +16,10 @@ struct dog
 	char *owner;
 
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
