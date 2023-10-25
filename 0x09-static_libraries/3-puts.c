5f21781d377331eee4d22b7202b9ab8af22010de
Nsikak-mbre <136088074+Nsikak-mbre@users.noreply.github.com>
2023-10-25 13:50:25 +0100

    Delete 0x09-static_libraries directory

--- 0x09-static_libraries/3-puts.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- *  *  * * _puts - prints a string to stdout
- *   *   * * @str: pointer to the string to print
- **/
-
-void _puts(char *str)
-{
-}
--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -Wno-unused-parameter -Wno-unused-result -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
