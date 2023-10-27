77b3b1c290b6480168328d16f1a7069732c0cde5
NsikakMbre <tm260715@gmail.com>
2023-10-27 09:13:25 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; 1 <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
@@ -30,5 +30,6 @@ int main(void)
 		}
 	}
 	printf("\n");
+    return (0);
 }
 
