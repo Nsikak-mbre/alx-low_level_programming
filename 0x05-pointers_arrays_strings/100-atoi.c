569534d1aa87d7db052693985a3b442982177c52
Nsikak-mbre <tm260715@gmail.com>
2023-10-28 19:09:09 +0100

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,15 +10,17 @@ int _atoi(char *s)
 	int sign = 1;
 	int num = 0;
 
-	while (*s == ' ' || (*s == '-' || *s == '+'))
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
 	{
-		if (*s == '_')
+		if (*s == '-')
 			sign *= -1;
+		if (*s == '+')
+			sign *= +1;
 		s++;
 	}
-	while (*s >= '0' &&  *s <= '9')
+	while ('0' <= *s && *s <= '9' && *s != '\0')
 	{
-		num = num * 10 + (*s - '0');
+		num = (num * 10) + (*s - '0');
 		s++;
 	}
 	return (num * sign);
