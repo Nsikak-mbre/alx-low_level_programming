5939dab641d7369f7761c455c8a2905d3197839b
Nsikak-mbre <tm260715@gmail.com>
2023-10-29 22:59:47 +0100

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - What's my number?
+ * @argc: My number
+ * @argv: My name
+ * Return: My Number
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	{
+		printf("%i\n", argc - 1);
+	}
+	return (0);
+}
