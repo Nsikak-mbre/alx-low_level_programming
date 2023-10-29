f53e2fa2f3fa6e6b2fd9b74e4a6694a347e478ca
Nsikak-mbre <tm260715@gmail.com>
2023-10-29 21:49:05 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Whats_my_name
+ * @argc: Quanity of items
+ * @argv: list of items
+ * Return: first argument
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
