d134ef7715cbb593c9b5d8763f5dc4eff7c3f380
Nsikak-mbre <tm260715@gmail.com>
2023-11-05 00:07:23 +0100

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *joined;
 	size_t s1_len = 0, s2_len = 0, count;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	joined[count] = '\0';
 
-	return joined;
+	return (joined);
 }
 
 
