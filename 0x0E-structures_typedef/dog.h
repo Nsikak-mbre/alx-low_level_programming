59c0c3c29a13f6e5bd6f42aee8678b3be2564d46
Nsikak-mbre <tm260715@gmail.com>
2023-11-06 16:42:39 +0100

    dog-struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+/**
+ * struct dog - dog
+ * @name: first member
+ * @owner: second member
+ * @age: third member
+ * Description : some dog attributes
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
