b3d6b3cf48c130d93744f5aad9fc01d36c9f931d
Nsikak-mbre <tm260715@gmail.com>
2023-11-07 04:42:56 +0100

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog -  Access and prints struct memebers
+ * @d: points to dog struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!d->name)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name : %s\n", d->name);
+		}
+		printf("Age: %f\n", d->age);
+		if (d->owner)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
+	}
+}
