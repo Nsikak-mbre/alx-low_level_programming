2a1f6a6efcb7430c326f79d50aacea00da607f3d
Nsikak-mbre <tm260715@gmail.com>
2023-11-07 22:59:14 +0100

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - new dog
+ * @name: New dog's name
+ * @age: New dog's age
+ * @owner: Dod's owner
+ * Return: New-dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int count, dog_name, dog_owner;
+
+	dog = malloc(sizeof(*dog));
+	if (dog == NULL || !(name) || !(owner))
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (dog_name = 0; name[dog_name]; dog_name++)
+		;
+	for (dog_owner = 0; owner[dog_owner]; dog_owner++)
+		;
+	if (!(dog->name) || !(dog->owner))
+
+	{
+		free(dog->owner);
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (count = 0; count < dog_name; count++)
+		dog->name[count] = name[count];
+	dog->name[count] = '\0';
+
+	dog->age = age;
+
+	for (count = 0; count < dog_owner; count++)
+		dog->owner[count] = owner[count];
+	dog->owner[count] = '\0';
+	return (dog);
+}
+
+
