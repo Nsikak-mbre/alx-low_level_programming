f7f709e8b43dbe8705cfec8687fcb19e252d3cf2
Nsikak-mbre <tm260715@gmail.com>
2023-11-08 14:55:21 +0100

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "function_pointers.h"
+/**
+ * print_name - outputs user name
+ * @name: holds name to output
+ * @f: pointer to name array
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name)
-	f (name);
+	f(name);
 }
