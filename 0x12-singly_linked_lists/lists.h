629a6eccebc1be438f73b6cf297fdf8103adbfde
Nsikak-mbre <tm260715@gmail.com>
2023-11-19 09:41:50 +0100

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,10 +11,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list _s;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
