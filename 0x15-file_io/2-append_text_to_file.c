32929baf0b68f2ff25cf4ed82354d770596faa78
Nsikak-mbre <tm260715@gmail.com>
2023-12-02 09:55:09 +0100

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 		cpy_wrd = write(desc, text_content, len);
 
-		if(cpy_wrd == -1)
+		if (cpy_wrd == -1)
 			return (-1);
 	}
 	close(desc);
