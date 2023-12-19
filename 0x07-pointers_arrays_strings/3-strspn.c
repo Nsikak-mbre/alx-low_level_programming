f0e8be4f42b0f74575cb7978890c24d199b2bf40
Nsikak-mbre <tm260715@gmail.com>
2023-12-19 02:31:52 +0100

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,16 @@
 #include "main.h"
+/**
+ * _strspn - checks for occurence len in bytes
+ * @s: charcter to look for
+ * @accept: where to search for above character
+ * Return: lent of found character in byte
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	int i,j;
 	unsigned int count = 0;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
@@ -16,5 +24,5 @@ unsigned int _strspn(char *s, char *accept)
 		if (accept[j] == '\0')
 			break;
 	}
-	return count;
+	return (count);
 }
