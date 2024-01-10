e6160b3527433607aaaaf6e7d9d668f1a0e21972
Nsikak-mbre <tm260715@gmail.com>
2024-01-10 14:58:02 +0100

    redo

---
+++ 0x0F-function_pointers/main.c
@@ -0,0 +1,13 @@
+#include "3-calc.h"
+
+/**
+ * main - calculator functions
+ * @argc: argument count
+ * @argv: array of argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int 
+
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,30 @@
 #ifndef __VARIADIC_FUNCTIONS_H__
 #define __VARIADIC_FUNCTIONS_H__
+#include <stdarg.h>
+/**
+ * FormatSpecifier - typedef
+ */
+typedef struct FormatSpecifier formatSpecifiers
 
+/**
+ * struct - Format specifiers
+ * @formatSpecifier: character representing the format
+ * @printFunction: pointer to a printing function
+ */
+struct FormatSpecifier
+{
+	char formatSpecifier;
+	void (*printFunction)(va_list);
+};
+
+void printChar(va_list valist);
+void printInt(va_list valist); 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void printFloat(va_list valist);
+void printString(va_list valist);
+
 #endif
