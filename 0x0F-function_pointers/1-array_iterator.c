706e644f31dafff6311af34a325646dced17fcfc
Nsikak-mbre <tm260715@gmail.com>
2024-01-26 11:25:14 +0100

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,7 +4,7 @@
  * array_iterator - interacts with each element in array
  * @array: contains element
  * @size: Unsigned int, portraying array size
- * @action: pointer to function 
+ * @action: pointer to function
  * Return: 0 on success
  */
 
