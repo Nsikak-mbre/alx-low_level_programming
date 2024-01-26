416e18cfd254669ddb99467b3c642ca0f12a16e5
Nsikak-mbre <tm260715@gmail.com>
2024-01-26 10:52:03 +0100

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#include <stdlib.h>
 #ifndef __FUNCTION_POINTER_H__
 #define __FUNCTION_POINTER_H__
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
