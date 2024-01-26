c649e802826cbc2103b1bfbc86bc83da78cb9192
Nsikak-mbre <tm260715@gmail.com>
2024-01-26 13:35:21 +0100

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - conpares elements index
+ * @array: contains elements
+ * @size: self explanatory
+ * @cmp: points to comparison function
+ * Return: 0 on success
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, index = -1;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) !=  0)
+		{
+			index = i;
+			break;
+		}
+	}
+	return (index);
+}
+
