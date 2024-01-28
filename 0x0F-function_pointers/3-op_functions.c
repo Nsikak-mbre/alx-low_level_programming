a848bfbd43b9d8eb6881aff84e18c87158cfda40
Nsikak-mbre <tm260715@gmail.com>
2024-01-28 07:46:19 +0100

    done

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -9,7 +9,7 @@
 
 int op_add(int a, int b)
 {
-	return a + b;
+	return (a + b);
 }
 
 /**
@@ -20,7 +20,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-	return a - b;
+	return (a - b);
 }
 
 
@@ -32,7 +32,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
 /**
  * op_div - Performs Division
@@ -42,7 +42,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	return a / b;
+	return (a / b);
 }
 /**
  * op_mod - returns remainder after a division
@@ -52,7 +52,7 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	return a % b;
+	return (a % b);
 }
 
 
