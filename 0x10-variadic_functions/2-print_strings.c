822cdace2d09040e193f3c9cf6feafe30d008898
Nsikak-mbre <tm260715@gmail.com>
2024-01-29 15:06:32 +0100

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,47 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+
+/**
+ * print_strings - self explanatory
+ * @separator: plcaed in=between strings
+ * @n: arg count
+ * Return: 0 on success
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	size_t i;
+	const char *temp;
+
+	if (separator == NULL)
+	{
+		return;
+	}
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		temp = va_arg(ptr, const char *);
+
+		if (temp != NULL)
+		{
+			printf("%s", temp);
+		}
+		else
+		{
+			printf("nil");
+		}
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+
+	}
+
+		va_end(ptr);
+		printf("\n");
+}
+
+
