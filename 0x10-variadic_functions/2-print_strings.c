2a4f9c28d51fbb88eec1c21627338f8bea48fa9f
Nsikak-mbre <tm260715@gmail.com>
2024-01-29 15:09:46 +0100

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ptr;
 	size_t i;
-	const char *temp;
+	char *temp;
 
 	if (separator == NULL)
 	{
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 	{
-		temp = va_arg(ptr, const char *);
+		temp = va_arg(ptr, char *);
 
 		if (temp != NULL)
 		{
