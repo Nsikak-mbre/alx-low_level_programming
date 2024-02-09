8567f7a768926241ba77eba9dbd95ae19af11591
Nsikak-mbre <tm260715@gmail.com>
2024-02-09 13:01:32 +0100

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees up allocated node spaces
+ * @head: pointer to first node
+ * Return: 0 on success
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
