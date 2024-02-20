3bd47aa6140c1d239ebf85510f35226c92607431
Nsikak-mbre <tm260715@gmail.com>
2024-02-20 23:19:22 +0100

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,9 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
+#define BUFF_SIZE 1024
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
