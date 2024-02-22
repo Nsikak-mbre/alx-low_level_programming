91ff75e3b1f8aefe65d00a822e71f87a1d1c6e4a
Nsikak-mbre <tm260715@gmail.com>
2024-02-22 22:12:13 +0100

    3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _memset -  copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - Allocates memory for an array
+ * @nmemb: number of elements
+ * @size: size of each bit
+ * Return: newly allocated mem
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *first_elem_ptr;
+	size_t total_elem_size, i;
+
+	if ((nmemb || size) == 0)
+		return (NULL);
+
+	total_elem_size = nmemb * size;
+	first_elem_ptr = malloc(each_element);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(first_elem_ptr, 0, total_elem_size);
+
+	return (first_elem_size);
+}
