ca4b11e0c2840c4b87a8137153e86bf3954f248d
Nsikak-mbre <tm260715@gmail.com>
2024-02-27 12:59:54 +0100

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - return a given node
+ * @head: points to first node
+ * @index: position of node to return
+ * Return: Null if failed, struct node on succes
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = head;
+	size_t count = 0;
+
+	while (current != NULL && count != index)
+	{
+		current = current->next;
+		count++;
+	}
+	if (count == index)
+		return (current);
+	else
+		return (NULL);
+	
+}
