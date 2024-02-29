eb413fbb5892b71e8f5b77ea8292392f2cc8bfac
Nsikak-mbre <tm260715@gmail.com>
2024-02-29 22:00:05 +0100

    103-find_loop.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct listint_s
 } listint_t;
 
 size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 size_t print_listint_safe(const listint_t *head);
