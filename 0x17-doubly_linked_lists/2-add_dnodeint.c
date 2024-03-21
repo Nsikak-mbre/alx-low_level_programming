4b7808cc2171e3f0d493c7a3439a522d97b14560
Nsikak-mbre <tm260715@gmail.com>
2024-03-21 21:02:24 +0100

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,12 +13,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node->prev = NULL;
+
 	if (*head == NULL)
 	{
+		new_node->next = NULL;
 		*head = new_node;
 	}
 	else
+	{
 		new_node->next = *head;
-	*head = new_node;
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
 	return (new_node);
 }
