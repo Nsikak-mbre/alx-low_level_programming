07fad3d8dd6875eff0385e7c0c694b8edbe23c50
Nsikak-mbre <tm260715@gmail.com>
2024-03-22 14:39:53 +0100

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,6 +41,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = current;
 	new_node->n = n;
 	new_node->next = current->next;
-	current->next->prev = new_node;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
 	return (new_node);
 }
