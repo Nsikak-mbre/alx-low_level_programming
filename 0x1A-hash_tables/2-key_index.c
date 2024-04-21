f4109397bae28a358fc4d6fd30272a51cb468647
Nsikak-mbre <tm260715@gmail.com>
2024-04-21 14:55:39 +0100

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * key index - Creates an index using object's key
+ * key_index - Creates an index using object's key
  * @key: Object's key
  * @size: Object size
  * Return: Index at which key/value should be stored
@@ -9,5 +9,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	size_t hash_value =  hash_djb2(key);
 	size_t index  = hash_value % size;
+
 	return (index);
 }
